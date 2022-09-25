9e0895b85d1f30ef99275f3e95e157218123b18e
Ohm Patel <ohmpate114@ed.amdsb.ca>
2022-09-24 20:41:09 -0400

    Initial commit

---
+++ src/Task3.java
@@ -0,0 +1,14 @@
+public class Task3 {
+    public static void main(String[] args) {
+
+//String with name "Joe Mahoney"
+        String name = "Joe Mahoney";
+        //int age with the value of 26
+        int age = 26;
+        //double annualPay with the value of 100000
+        double annualPay = 100000.0;
+
+        //Prints name, age, and annual pay for Joe Mahoney
+        System.out.println("My name is " + name + ", my age is " + age + " and \nI hope to earn " + annualPay + " per year.");
+    }
+}
